37724567414b333e150a5d4df5dde4c39f89db93
shruthika-tr <shruthikaramesh2004@gmail.com>
2025-12-31 13:52:48 +0530

    Fix Render port binding

--- Server/app.py
+++ Server/app.py
@@ -201,6 +201,6 @@ def history(service):
 # -----------------------
 # Run (Render-compatible)
 # -----------------------
-if __name__ == "__main__":
+##if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
